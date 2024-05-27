2431013175bce30f5f5b111df620222f2d33226d
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-05-27 21:21:37 +0100

    Update Go script

--- update/main.go
+++ update/main.go
@@ -17,7 +17,7 @@ func main() {
 	const aboutText = `<p>I'm a Backend Engineer working in cybersecurity, with over 5 years of experience in the fintech industry. I have a strong interest in automation and improving process efficiency. Right now I use Go, Python, JavaScript, GraphQL, SQL and Cassandra in my day-to-day work and side projects.</p>`
 	const latestPostsTitle = `<h2>📬 Latest posts</h2>`
 	const techStackTitle = `<h2>💻 Tech Stack</h2>`
-	const techStackText = `![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![GraphQL](https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white) ![ApacheCassandra](https://img.shields.io/badge/cassandra-%231287B1.svg?style=for-the-badge&logo=apache-cassandra&logoColor=white) ![MySQL](https://img.shields.io/badge/mysql-%2300000f.svg?style=for-the-badge&logo=mysql&logoColor=white) ![AWS](https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white) ![Hugo](https://img.shields.io/badge/Hugo-black.svg?style=for-the-badge&logo=Hugo) ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white)`
+	const techStackText = `![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![GraphQL](https://img.shields.io/badge/-GraphQL-E10098?style=for-the-badge&logo=graphql&logoColor=white) ![ApacheCassandra](https://img.shields.io/badge/cassandra-%231287B1.svg?style=for-the-badge&logo=apache-cassandra&logoColor=white) ![MySQL](https://img.shields.io/badge/mysql-%2300000f.svg?style=for-the-badge&logo=mysql&logoColor=white) ![AWS](https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white) ![Hugo](https://img.shields.io/badge/Hugo-black.svg?style=for-the-badge&logo=Hugo) ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white)`
 	const githubStatsTitle = `<h2>📊 GitHub Stats</h2>`
 	const githubStatsText = `![](https://github-readme-stats.vercel.app/api?username=Skyth3r&theme=city_light&hide_border=false&include_all_commits=false&count_private=true)<br/>![](https://github-readme-streak-stats.herokuapp.com/?user=Skyth3r&theme=city_light&hide_border=false)`
 	const githubContributionsTitle = `<h2>📈 GitHub Contributions</h2>`
@@ -33,8 +33,8 @@ func main() {
 	webFeedItem := getLatestFeedItem(webFeed)
 	devFeedItem := getLatestFeedItem(devFeed)
 
-	article := `<p>Latest article from my website: <a href="` + webFeedItem.Link + `">` + webFeedItem.Title + `</a>. Subscribe to <a href="` + webFeed + `">article RSS feed.</a></p>`
-	devArticle := `<p>Latest post from my dev blog: <a href="` + devFeedItem.Link + `">` + devFeedItem.Title + `</a>. Subscribe to <a href="` + devFeed + `">dev post RSS feed.</a></p>`
+	article := `<p>Latest article from my website: <a href="` + webFeedItem.Link + `">` + webFeedItem.Title + `</a>.`
+	devArticle := `<p>Latest post from my dev blog: <a href="` + devFeedItem.Link + `">` + devFeedItem.Title + `</a>.`
 
 	date := time.Now().Format("2 Jan 2006")
 	updated := `<sub>Last updated on ` + date + `.<sub>`
